71c1afe632f647e6ae42c6174e71674a029ee6ca
Matheus José <matheus.jose@i9xp.com.br>
2021-05-19 00:43:35 -0300

    initial

--- config/database.js
+++ config/database.js
@@ -1,11 +1,13 @@
+const uri = process.env.MONGODB_PRODUCTION || `mongodb://${process.env.HOST_MONGODB}:${process.env.PORT_MONGODB}/${process.env.DATABASE_MONGODB}`
+
 module.exports = {
-    uri: `mongodb://${process.env.HOST_MONGODB}:${process.env.PORT_MONGODB}/${process.env.DATABASE_MONGODB}`,
+    uri: uri,
     options: {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        poolSize: 10, // Manter 10 sockets de conexão
-        serverSelectionTimeoutMS: 5000, // Tenta enviar operação por 5 segundos antes de dar timeout
-        socketTimeoutMS: 45000, // Fecha conexão depois de 45 segundos inativo
+        poolSize: 10, // Manter 10 sockets de conexï¿½o
+        serverSelectionTimeoutMS: 5000, // Tenta enviar operaï¿½ï¿½o por 5 segundos antes de dar timeout
+        socketTimeoutMS: 45000, // Fecha conexï¿½o depois de 45 segundos inativo
         family: 4, // Usa IPv4, pulando tentativa de uso do IPv6
         auth: {
             authSource: 'admin'
