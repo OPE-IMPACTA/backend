d35ff2276bd685fc19447db969e4c298dfd58044
Matheus José <matheus.jose@i9xp.com.br>
2021-05-19 00:55:18 -0300

    make it better

--- config/database.js
+++ config/database.js
@@ -8,11 +8,7 @@ module.exports = {
         poolSize: 10, // Manter 10 sockets de conex�o
         serverSelectionTimeoutMS: 5000, // Tenta enviar opera��o por 5 segundos antes de dar timeout
         socketTimeoutMS: 45000, // Fecha conex�o depois de 45 segundos inativo
-        family: 4, // Usa IPv4, pulando tentativa de uso do IPv6
-        auth: {
-            authSource: 'admin'
-        },
-        user: process.env.MONGO_INITDB_ROOT_USERNAME,
-        pass: process.env.MONGO_INITDB_ROOT_PASSWORD,
+        family: 4 // Usa IPv4, pulando tentativa de uso do IPv6
+
     }
 };
